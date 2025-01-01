96f2f0593b8b7e177cdbd6664cc13efe81507c06
GabsDevJunior <gladiadorrubi@gmail.com>
2025-01-01 19:48:34 -0300

    adicionei o pulo e o physics material 2D.

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -6,19 +6,43 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D rig;
 
-    public Vector2 velocity;
-    
     public float speed;
-void Update()
+    public float forcejump;
+
+    public Vector2 friction = new Vector2 (1f, 0f);
+    void Update()
     {
-        if(Input.GetKey(KeyCode.D))
+        jump();
+        walk();
+    }
+
+    void walk()
+    {
+        if (Input.GetKey(KeyCode.D))
         {
-            rig.velocity = new Vector2(speed, 0);
+            rig.velocity = new Vector2(speed, rig.velocity.y);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            rig.velocity = new Vector2(-speed, 0);
+            rig.velocity = new Vector2(-speed, rig.velocity.y);
+        }
+
+        if(rig.velocity.x > 0)
+        {
+            rig.velocity += friction;
+        }
+
+        if (rig.velocity.x < 0)
+        {
+            rig.velocity -= friction;
+        }
+    }
+    void jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.velocity = Vector2.up * forcejump;
         }
     }
 }
