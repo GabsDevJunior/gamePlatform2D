445c296fb3e2d164b44fbd69dd55d70710370202
GabsDevJunior <gladiadorrubi@gmail.com>
2025-01-02 20:53:12 -0300

    fiz pulo e o pulo duplo

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEditor;
 
 public class Player : MonoBehaviour
 {
@@ -10,10 +12,23 @@ public class Player : MonoBehaviour
     public float forcejump;
 
     public Vector2 friction = new Vector2 (1f, 0f);
+
+    [Header("Animation")]
+    public float JumpScaleY = 1.5f;
+    public float JumpScaleX = .7f;
+    public float GroundAnimationY = 1.5f;
+    public float GroundAnimationX = .5f;
+    public float AnimationDurationJump;
+    public float AnimationDurationGround;
+    public Ease ease = Ease.OutBack;
+
+    public bool onJump;
+    public bool onDoubleJump;
     void Update()
     {
         jump();
         walk();
+        
     }
 
     void walk()
@@ -42,7 +57,70 @@ public class Player : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            rig.velocity = Vector2.up * forcejump;
+            if (!onJump)
+            {
+                rig.velocity = Vector2.up * forcejump;
+                onJump = true;
+                onDoubleJump = false;
+                rig.transform.localScale = Vector2.one;
+
+                DOTween.Kill(rig.transform);
+                ScaleJump();
+            }
+            else if(!onDoubleJump)
+            {
+                rig.velocity = Vector2.up * forcejump;
+                    onDoubleJump = true;
+                    rig.transform.localScale = Vector2.one;
+
+                DOTween.Kill(rig.transform);
+                ScaleJump();
+            }
+        }
+    }
+
+
+
+    void ScaleJump()
+    {
+        rig.transform.DOScaleY(JumpScaleY, AnimationDurationJump).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        rig.transform.DOScaleX(JumpScaleX, AnimationDurationJump).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+    }
+
+    void animGround()
+    {
+        rig.transform.DOScaleY(GroundAnimationY, AnimationDurationGround).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        rig.transform.DOScaleX(GroundAnimationX, AnimationDurationGround).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+    }
+
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 9)
+        {
+
+            rig.transform.localScale = Vector2.one;
+
+            DOTween.Kill(rig.transform);
+            animGround();
+            onJump = false;
+            onDoubleJump = true;
+            
+
+
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {  
+
+
+            onJump = true;
+            onDoubleJump = false;
         }
     }
 }
